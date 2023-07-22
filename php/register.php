5865e8828da54e1afd50d356a6488369fd93a5d6
Reshma k Pillai <reshmakpillai3@gmail.com>
2023-07-21 20:43:12 -0400

    reg and login update

--- php/register.php
+++ php/register.php
@@ -1,16 +1,20 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-	$name = $_POST["name"];
-	$username = $_POST["username"];
+	$firstname = $_POST["firstname"];
+	$lastname = $_POST["lastname"];
 	$email = $_POST["email"];
+	$regnumber = $_POST["regnumber"];
+	$age = $_POST["age"];
+	$gender = $_POST["gender"];
+	$username = $_POST["username"];
 	$password = $_POST["password"];
-	
+
 	// Hash the password
 	$hashed_password = password_hash($password, PASSWORD_BCRYPT);
 	$host = "localhost";
-	$dbname = "Travel";
-	$username_db = "root";
-	$password_db = "";
+	$dbname = "travel";
+	$username_db = "travel";
+	$password_db = "travel";
 	try {
 		$db = new PDO(
 		"mysql:host=$host;dbname=$dbname",
@@ -19,15 +23,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 		
 		// Insert the user into the database
 		$stmt = $db->prepare(
-		"INSERT INTO users (name,username,email, password)
-			VALUES (:name, :username, :email,:password)");
-		$stmt->bindParam(":name", $name);
-		$stmt->bindParam(":username", $username);
+		"INSERT INTO users (firstname,lastname,email,phone_number,age,gender,username, password)
+			VALUES (:firstname,:lastname,:email,:phone_number,:age,:gender, :username,:password)");
+		$stmt->bindParam(":firstname", $firstname);
+		$stmt->bindParam(":lastname", $lastname);
 		$stmt->bindParam(":email", $email);
+		$stmt->bindParam(":phone_number", $regnumber);
+		$stmt->bindParam(":age", $age);
+		$stmt->bindParam(":gender", $gender);
+		$stmt->bindParam(":username", $username);
 		$stmt->bindParam(":password", $hashed_password);
 		$stmt->execute();
 		echo "<h2>Registration Successful</h2>";
-		echo "Thank you for registering, " . $name . "!<br>";
+		echo "Thank you for registering, " . $firstname . "!<br>";
 		echo "You'll be redirected to login page in 3 seconds";
 		header("refresh:3;url=../Html/login.html");
 	}
