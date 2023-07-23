3b86a02bed1de8dada5bc5d5cc695662910e1f5d
Anirudh <anirudhkarma@gmail.com>
2023-07-23 14:19:16 -0400

    File arrangement

---
+++ js/script.js
@@ -0,0 +1,100 @@
+var swiper = new Swiper(".home-slider", {
+    loop:true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+ });
+ 
+ var swiper = new Swiper(".reviews-slider", {
+    grabCursor:true,
+    loop:true,
+    autoHeight:true,
+    spaceBetween: 20,
+    breakpoints: {
+       0: {
+         slidesPerView: 1,
+       },
+       700: {
+         slidesPerView: 2,
+       },
+       1000: {
+         slidesPerView: 3,
+       },
+    },
+ });
+ 
+
+    var dialogLogin = document.getElementById("dialogLogin");
+    var dialogRegsiter = document.getElementById("dialogRegsiter");
+
+    function openDialogLogin() {
+        dialogLogin.style.display = "block";
+        dialogRegsiter.style.display = "none";
+    }
+    function openDialogRegsiter() {
+        dialogRegsiter.style.display = "block";
+        dialogLogin.style.display = "none";
+    }
+$("document").ready(function() {
+    // Swipe Animation
+    var swipeContent = $('.swipe-content');
+    var swipeContainer = $('.swipe-container');
+    var swipeWidth = swipeContent.outerWidth();
+    var containerWidth = swipeContainer.width();
+    var position = 0;
+
+    $('.swipe-arrow.next').click(function() {
+        position -= containerWidth;
+        if (position < -swipeWidth + containerWidth) {
+            position = -swipeWidth + containerWidth;
+        }
+        swipeContent.css('transform', 'translateX(' + position + 'px)');
+    });
+
+
+    $('.swipe-arrow.prev').click(function() {
+        position += containerWidth;
+        if (position > 0) {
+            position = 0;
+        }
+        swipeContent.css('transform', 'translateX(' + position + 'px)');
+    });
+});
+
+
+    // Open login dialog
+    function openDialogLogin() {
+        document.getElementById("dialogLogin").style.display = "block";
+    }
+
+    // Close login dialog when clicking outside the dialog box
+    window.onclick = function(event) {
+        var dialogLogin = document.getElementById("dialogLogin");
+        if (event.target === dialogLogin) {
+            dialogLogin.style.display = "none";
+        }
+    }
+
+    // Close login dialog when clicking on the close icon
+    function closeDialogLogin() {
+        document.getElementById("dialogLogin").style.display = "none";
+    }
+
+    // Open registration dialog
+    function openDialogRegister() {
+        document.getElementById("dialogRegister").style.display = "block";
+    }
+
+    // Close registration dialog when clicking outside the dialog box
+    window.onclick = function(event) {
+        var dialogRegister = document.getElementById("dialogRegister");
+        if (event.target === dialogRegister) {
+            dialogRegister.style.display = "none";
+        }
+    }
+
+    // Close registration dialog when clicking on the close icon
+    function closeDialogRegister() {
+        document.getElementById("dialogRegister").style.display = "none";
+    }
