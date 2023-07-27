f9205c69e94df61959b79bb6e2450a336c67be6c
Anirudh <anirudhkarma@gmail.com>
2023-07-27 08:07:10 -0400

    About page and package page

--- js/script.js
+++ js/script.js
@@ -62,6 +62,20 @@ $("document").ready(function() {
     });
 });
 
+// Load more button 
+let loadMoreBtn = document.querySelector('.packages .load-more .btn');
+let currentItem = 3;
+
+loadMoreBtn.onclick = () =>{
+   let boxes = [...document.querySelectorAll('.packages .box-container .box')];
+   for (var i = currentItem; i < currentItem + 3; i++){
+      boxes[i].style.display = 'inline-block';
+   };
+   currentItem += 3;
+   if(currentItem >= boxes.length){
+      loadMoreBtn.style.display = 'none';
+   }
+}
 
     // Open login dialog
     function openDialogLogin() {
@@ -98,3 +112,22 @@ $("document").ready(function() {
     function closeDialogRegister() {
         document.getElementById("dialogRegister").style.display = "none";
     }
+
+    // reviews area script
+    var swiper = new Swiper(".reviews-slider", {
+        grabCursor:true,
+        loop:true,
+        autoHeight:true,
+        spaceBetween: 20,
+        breakpoints: {
+           0: {
+             slidesPerView: 1,
+           },
+           700: {
+             slidesPerView: 2,
+           },
+           1000: {
+             slidesPerView: 3,
+           },
+        },
+     });
